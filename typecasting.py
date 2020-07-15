ad3c3611ed00727ac37f069c4593ab4d5f1c3fbb
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-15 19:01:54 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+var1 = 'hello'
+var2 = 32
+var3 = 36.7
+print(type(var1))
+print(type(var2))
+print(type(var3))
+#typecasting
+var1 = "52"
+var2 = "34"
+print((var1)+var2)
+#typecasting
+print(int(var1)+int(var2))
+var1 = "54"
+var2 = "32"
+print(10*int(var1) + int(var2))
+#wanna print any thing multiple tyms then we do
+print(10 * "hello\n")
+#now we wanna add strings and print it 10 times
+var1 = '54'
+var2 = '32'
+print(10 * str (int(var1) + int(var2)))
