e07b48fd4c52716f876db4836793f7af9ab3c507
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-15 19:21:55 +0530

    Add files via upload

---
+++ sum of 2 no.py
@@ -0,0 +1,18 @@
+#to take input from user
+#whatever no. u enter it will store as a string
+print("enter ur no.")
+n = input()
+print("you entered",n)
+#now we wanna add 10 in above program, it will show error
+#print("enter ur number")
+#n = input()
+#print("sum is",n+10)
+#after using typecasting
+print("enter ur no.")
+n = input()
+print("sum after adding 10 is",int(n)+10)
+#to make calculator and add 2 no.
+print("enter a no.")
+n1 = input()
+n2 = input()
+print("sum of 2 no. is",int(n1) + int(n2))
